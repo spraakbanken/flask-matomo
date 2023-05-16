0f9eab5ac757a0897fb083f10a9c417d76d28bd5
Kristoffer Andersson <kristoffer.andersson@gu.se>
2023-05-16 10:59:30 +0200

    chore: silence lint

--- flask_matomo/core.py
+++ flask_matomo/core.py
@@ -1,15 +1,12 @@
+import typing
 import urllib.parse
 from threading import Thread
 
 import httpx
+from flask import _request_ctx_stack as stack
 from flask import current_app, request
 
 from . import MatomoError
-
-try:
-    from flask import _app_ctx_stack as stack
-except ImportError:
-    from flask import _request_ctx_stack as stack
 
 
 class Matomo(object):
@@ -38,9 +35,9 @@ class Matomo(object):
         self.id_site = id_site
         self.token_auth = token_auth
         self.base_url = base_url.strip("/") if base_url else base_url
-        self.ignored_routes = []
         self.ignored_ua_prefixes = []
-        self.routes_details = {}
+        self.ignored_routes: typing.List[str] = []
+        self.routes_details: typing.Dict[str, typing.Dict[str, str]] = {}
         self.client = client or httpx.Client()
 
         if not matomo_url:
@@ -86,9 +83,9 @@ class Matomo(object):
         }
 
         # Overwrite action_name, if it was configured with config()
-        if self.routes_details.get(action_name) and self.routes_details.get(
-            action_name
-        ).get("action_name"):
+        if self.routes_details.get(action_name) and self.routes_details.get(action_name).get(
+            "action_name"
+        ):
             keyword_arguments["action_name"] = self.routes_details.get(action_name).get(
                 "action_name"
             )
@@ -96,7 +93,7 @@ class Matomo(object):
         # Create new thread with request, because otherwise the original request will be blocked
         Thread(target=self.track, kwargs=keyword_arguments).start()
 
-    def track(self, action_name, url, user_agent=None, id=None, ip_address=None):
+    def track(self, action_name, url, user_agent=None, id=None, ip_address=None):  # noqa: A002
         """Send request to Matomo
 
         Args:
